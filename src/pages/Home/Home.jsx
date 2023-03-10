08607fa59f638dd9c98896f497800ee2c902e87e
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-10 09:54:21 -0300

    changing styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { Container, Cards, Card } from '../styles'
 import { imageUrl, TopRatedUrl } from '../../config'
 
 const Home = () => {
-    
+
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
@@ -17,17 +17,16 @@ const Home = () => {
             <section>
                 <h1>Top Rated</h1>
                 <Cards>
-                    {movies.map(movie => {
-                        return (
-                            <Card key={movie.id}>
-                                <a href="#">
-                                    <img src={`${imageUrl}${movie.poster_path}`} alt={movie.title} />
-                                </a>
-                                <span>{movie.title}</span>
-                            </Card>
-                        )
-                    })}
+                    {movies.map(movie => (
+                        <Card key={movie.id}>
+                            <a href="#">
+                                <img src={`${imageUrl}${movie.poster_path}`} alt={movie.title} />
+                            </a>
+                            <span>{movie.title}</span>
+                        </Card>
+                    ))}
                 </Cards>
+
             </section>
         </Container>
     )
--- src/pages/styles.jsx
+++ src/pages/styles.jsx
@@ -10,13 +10,13 @@ export const Cards = styled.ul`
     align-items: center;
     justify-content: space-around;
     padding: 1.5rem;
-    flex-wrap: wrap;
     row-gap: 4rem;
 `
 
 export const Card = styled.li`
     display: flex;
     align-items: center;
+    text-aligbn: center;
     flex-direction: column;
     margin-bottom: 1.25rem;
     margin-left: 1.4rem;
