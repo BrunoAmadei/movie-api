5c4da3a252bd64a60d216e97a64c32622824dccf
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-14 08:49:26 -0300

    setting routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './global.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Movie from './pages/movie/Movie'
+import Home from './pages/Home/Home'
+import Search from './pages/search/Search'
 
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />}>
-
+      <Route element={<App />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:id' element={<Movie />} />
+        <Route path='searc' element={<Search />} />
       </Route>
     </Routes>
   </BrowserRouter>,
--- src/pages/movie/Movie.jsx
+++ src/pages/movie/Movie.jsx
@@ -1,4 +1,8 @@
+import {useParams} from 'react-router-dom'
+
 const Movie = () => {  
+    const {id} = useParams()
+    console.log(id)
     return (
         <div>
             <h1>Página de detalhes de filmes</h1>
