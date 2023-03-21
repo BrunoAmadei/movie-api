749b3d243971fff3cf46e2eb9b28d7d3e5995522
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-21 09:32:02 -0300

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar, AiOutlineInfoCircle } from "react-icons/ai";
 import { Featured, BannerVertical, BannerHorizontal, Title, Info } from './styles'
-
+import {Link} from 'react-router-dom'
 export default ({ item }) => {
+    let genres = []
+
+    for (let g in item.genres) {
+        genres.push(item.genres[g].name)
+        console.log(item.genres[g].name)
+    }
+
     return (
         <Featured style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})` }}>
             <BannerVertical>
@@ -12,10 +19,7 @@ export default ({ item }) => {
                     </Title>
                     <Info>
                         <div>
-                            <span>
-                                <AiFillStar />
-                            </span>
-                            {item.vote_average}
+                            <span><AiFillStar /></span> {item.vote_average}
                         </div>
                         <div>
                             {item.release_date}
@@ -24,6 +28,14 @@ export default ({ item }) => {
                     </Info>
                     <div>
                         {item.overview}
+                    </div>
+                    <div>
+                        <Link>
+                            <button><AiOutlineInfoCircle/> Info</button>
+                        </Link>
+                    </div>
+                    <div>
+                        <strong>Genres:</strong> {genres.join(', ')}
                     </div>
                 </BannerHorizontal>
             </BannerVertical>
