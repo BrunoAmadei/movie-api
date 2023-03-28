8e676ef9097d81a3a420d4a9cd40bc3a909a6c6e
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-28 14:16:36 -0300

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { AiFillStar, AiFillCalendar, AiFillClockCircle } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { Featured, BannerVertical, BannerHorizontal, Title, Info, Vote, ReleaseDate, Runtime, Description, BtnArea, Genres } from './styles'
@@ -30,11 +31,11 @@ export default ({ item }) => {
                         <p>{item.overview.split(" ").slice(0, 30).join(" ")}...</p>
                     </Description>
                     <BtnArea>
-                        <a href={`/Movie/${item.id}`}>
+                        <Link to={`/Movie/${item.id}`}>
                             <button>
                                 <span><BsInfoCircle /></span> Info
                             </button>
-                        </a>
+                        </Link>
                     </BtnArea>
                     <Genres>
                         <strong>Genres:</strong> {genres.join(', ')}
